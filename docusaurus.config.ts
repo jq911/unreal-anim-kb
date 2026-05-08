13bebc4d022167ffe36ad09f7e7086df9911ce6a
AZu <62418220+jq911@users.noreply.github.com>
2026-05-08 03:56:58 +0100

    update: replace favicon config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
     },
     footer: {
       style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Unreal Animation Knowledge Base. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} AZu. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
